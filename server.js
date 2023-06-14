8a0c2f6549094847a68bfa9c31afdd327f4faad9
Xavi Font <xavifont99@gmail.com>
2023-06-13 20:52:32 -0600

    added test endpoint 2

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'tree.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.send("request has been reached");
 });
 
